fbc984b1888b0700e5b7f8b6a8eec2f92857b843
cabraham1 <christopherabraham8@gmail.com>
2023-05-26 16:19:55 +0100

    Add factorial

--- solver.rb
+++ solver.rb
@@ -1,2 +1,10 @@
 class Solver
+  def factorial(num)
+    raise ArgumentError, 'negative number are not allowed' if num.negative?
+
+    if [0, 1].include?(num)
+      1
+    else
+      num * factorial(num - 1)
+    end
 end
